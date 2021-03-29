a9f014a2ff702b5ecee064f397e25c97bc90e817
parabolic <nvelkovski@gmail.com>
2021-03-29 21:07:10 +0200

    Create_PubSub_and_sa_with_key - add a topic and subscription - create an SA and grant that SA role roles/pubsub.subscriber role - output the SA key

--- terraform/iac-pipelines-with-terraform-and-cloud-build/workloads/stag-3380426388/main.tf
+++ terraform/iac-pipelines-with-terraform-and-cloud-build/workloads/stag-3380426388/main.tf
@@ -35,3 +35,57 @@ resource "google_storage_bucket" "terraform_state" {
     enabled = true
   }
 }
+
+resource "google_pubsub_topic" "app_1" {
+  name = "app-1"
+
+  labels = {
+    app = "1"
+  }
+}
+
+resource "google_pubsub_subscription" "app_1" {
+  name  = "app-1"
+  topic = google_pubsub_topic.app_1.name
+
+  labels = {
+    app = "1"
+  }
+
+  message_retention_duration = "200s"
+  retain_acked_messages      = false
+
+  ack_deadline_seconds = 20
+
+  expiration_policy {
+    ttl = "300000.5s"
+  }
+
+  retry_policy {
+    minimum_backoff = "30s"
+  }
+
+  enable_message_ordering = false
+}
+
+resource "google_service_account" "app_1" {
+  account_id   = "app-1"
+  display_name = "app-1"
+}
+
+resource "google_service_account_key" "app_1" {
+  service_account_id = google_service_account.app_1.name
+}
+
+resource "google_pubsub_subscription_iam_binding" "" {
+  subscription = google_pubsub_subscription.app_1
+  role         = "roles/pubsub.subscriber"
+  members = [
+    "serviceAccount:${google_service_account.app_1}",
+  ]
+}
+
+output "app_1_sa_key" {
+  value     = google_service_account_key.app_1.private_key
+  sensitive = true
+}
